e2ad90b04ca597f082cdfb7575a56bd9dd71a272
Christian Guevara <cguevara@inovabiz.com>
2021-02-22 20:23:38 -0400

    se agrega validaciones personalizadas a los formularios

--- src/app/admin/components/form-product/form-product.component.ts
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 import { ProductsService } from '../../../core/services/products/products.service';
-import { Router } from '@angular/router';
+import { MyValidators } from '../../../utils/validators';
 
 @Component({
   selector: 'app-form-product',
@@ -22,25 +23,29 @@ export class FormProductComponent implements OnInit {
 
   ngOnInit(): void {}
 
+  private buildForm(): void {
+    this.form = this.formBuilder.group({
+      id: ['', [Validators.required]],
+      title: ['', [Validators.required]],
+      price: ['', [Validators.required, MyValidators.isPriceValid]],
+      image: ['', []],
+      description: ['', [Validators.required]],
+    });
+  }
+
   saveProduct(event: Event): void {
     event.preventDefault();
 
     if (this.form.valid) {
       const product = this.form.value;
-      this.productService.createProduct(product).subscribe((response) => {
+      this.productService.createProduct(product).subscribe((newProduct) => {
+        console.log('Producto enviado', product, 'nuevo Producto', newProduct);
         this.router.navigate(['./admin/products']);
       });
     }
-    console.log(this.form.value);
   }
 
-  private buildForm(): void {
-    this.form = this.formBuilder.group({
-      id: ['', [Validators.required]],
-      title: ['', [Validators.required]],
-      price: ['', [Validators.required]],
-      image: ['', []],
-      description: ['', [Validators.required]],
-    });
+  getError(inputName: string, errorName: string) {
+    return this.form.get(inputName).hasError(errorName);
   }
 }
