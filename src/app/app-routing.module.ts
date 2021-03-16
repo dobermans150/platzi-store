942f6c09c0d2e116cb4caf9da68a5b760a157994
Christian Guevara <cguevara@inovabiz.com>
2021-03-16 05:49:42 -0400

    Se actualiza el angular cli y core del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -68,8 +68,9 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      preloadingStrategy: PreloadAllModules,
-    }),
+    preloadingStrategy: PreloadAllModules,
+    relativeLinkResolution: 'legacy'
+}),
   ],
   exports: [RouterModule],
 })
