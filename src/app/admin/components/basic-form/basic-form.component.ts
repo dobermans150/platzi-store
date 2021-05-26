c0a979e88bdc9ed25de3869047c484ee0f2ceb62
Christian Guevara <christianjguevara@hotmail.com>
2021-05-26 10:49:12 -0400

    se agrega for groups anidados

--- src/app/admin/components/basic-form/basic-form.component.ts
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {
-  FormControl,
   Validators,
   FormGroup,
   AbstractControl,
@@ -30,21 +29,42 @@ export class BasicFormComponent implements OnInit {
 
   private buildFor(): void {
     this.form = this.formBuilder.group({
-      name: ['', [Validators.required, Validators.maxLength(10)]],
-      email: ['', Validators.required],
+      fullName: this.formBuilder.group({
+        name: [
+          '',
+          [
+            Validators.required,
+            Validators.maxLength(10),
+            Validators.pattern('^([Aa-zA-ZáéíóúÁÉÍÓÚÑñ]{2,}s?){2,4}$'),
+          ],
+        ],
+        last: [
+          '',
+          [
+            Validators.required,
+            Validators.maxLength(10),
+            Validators.pattern('^([Aa-zA-ZáéíóúÁÉÍÓÚÑñ]{2,}s?){2,4}$'),
+          ],
+        ],
+      }),
+      email: ['', [Validators.required, Validators.email]],
       phone: [''],
       color: ['#00000'],
       date: [''],
-      number: [''],
+      number: [
+        18,
+        [Validators.required, Validators.min(18), Validators.max(100)],
+      ],
       category: ['categoy-1'],
       tag: ['categoy-1'],
-      agree: [false],
+      agree: [false, [Validators.requiredTrue]],
       gender: ['male'],
       zone: [''],
     });
   }
 
-  getNameValue(): void {
+  getNameValue(event): void {
+    event.preventDefault();
     console.log(this.nameField.value);
   }
 
@@ -65,7 +85,11 @@ export class BasicFormComponent implements OnInit {
   }
 
   get nameField(): AbstractControl {
-    return this.form.get('name');
+    return this.form.get('fullName.name');
+  }
+
+  get lastNameField(): AbstractControl {
+    return this.form.get('fullName.last');
   }
 
   get emailField(): AbstractControl {
