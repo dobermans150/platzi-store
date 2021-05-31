47e743f88cc6787eca72545bb0ab9f87a190b5ef
Christian Guevara <christianjguevara@hotmail.com>
2021-05-31 14:43:53 -0400

    se agrega validaciones segun la interaccion en el ui

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -28,6 +28,7 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {}
 
   private buildForm(): void {
+    /* Formulario */
     this.form = this.formBuilder.group(
       {
         email: ['', [Validators.required]],
@@ -40,10 +41,25 @@ export class RegisterComponent implements OnInit {
           ],
         ],
         confirmPassword: ['', [Validators.required]],
+        type: ['company', [Validators.required]],
+        companyName: ['', [Validators.required]],
       },
       { validators: MyValidators.matchPassword }
     );
+
+    this.typeField.valueChanges.subscribe((value) => {
+      console.log(value);
+      if (value === 'company') {
+        this.companyNameField.setValidators([Validators.required]);
+      } else {
+        this.companyNameField.setValidators(null);
+      }
+
+      this.companyNameField.updateValueAndValidity();
+    });
   }
+
+  /* Functions */
 
   register(event): void {
     event.preventDefault();
@@ -55,12 +71,23 @@ export class RegisterComponent implements OnInit {
     }
   }
 
+  /* Getters */
+  get typeField(): AbstractControl {
+    return this.form.get('type');
+  }
+
+  get companyNameField(): AbstractControl {
+    return this.form.get('companyName');
+  }
+
   get emailField(): AbstractControl {
     return this.form.get('email');
   }
+
   get passwordField(): AbstractControl {
     return this.form.get('password');
   }
+
   get confirmPasswordField(): AbstractControl {
     return this.form.get('confirmPassword');
   }
